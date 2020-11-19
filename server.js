1f70725fcca6d2ba8b69b888afb7ae45c49748ce
Jordan Kirby <jrock255@hotmail.com>
2020-11-18 19:40:45 -0700

    database connected

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
