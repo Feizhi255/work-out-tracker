8c7e6c54f66a83c503bdd2580e52a1747970a3dc
Jordan Kirby <jrock255@hotmail.com>
2020-11-18 20:14:29 -0700

    removed some code for connecting to mongoD

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workouts',
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
     }
 );
 
